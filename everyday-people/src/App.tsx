30764cd004f8a1cf03a2bed54cd94706af2c45a7
Wouter V <wouter.vercruysse@zure.com>
2026-03-12 09:48:26 +0100

    first commit

---
+++ everyday-people/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ParticleRender from "./features/particle-render/ParticleRender";
+
+function App() {
+  return (
+    <>
+      <ParticleRender />
+    </>
+  );
+}
+
+export default App;
